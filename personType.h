89f0e9e1814a79ae7fd806e9b06b77ab01268994
jarvis <samman.thapa@bison.howard.edu>
2015-02-05 00:39:54 -0500

    starting the project

---
+++ personType.h
@@ -0,0 +1,24 @@
+using namespace std;
+class personType
+{
+public:
+	string name;
+	int person_no;
+	addressType address;
+	
+private:
+	float interest1, interest2;
+}
+class addressType
+{
+public:
+	char Gender;
+	string StreetAddress, StreetDec, State, StateInitials, ZipCode;
+	int StreetAddressNum;
+}
+
+
+	string lastName, firstName, p
+	  myfile >> members[i].lastName  >> members[i].firstName >> members [i].personNum >> 
+            members[i].personID >> members [i].address.streetAddressNum >> members[i].address.streetAddress >> members [i].address.streetDec >>  members[i].address.state >> 
+            members[i].address.stateInitials >>  members[i].address.zipCode >> members [i].gender;
